196dfec162a75607fe63be181cc680f4b3faf7f7
Jonathan Ragan-Kelley <jrk@google.com>
2016-12-15 18:32:13 -0800

    Several variations of blur test
    Includes a lot of ugly WIP cruft

--- Makefile
+++ Makefile
@@ -1,14 +1,32 @@
 HALIDE_ROOT ?= $(HOME)/hl-lin
-CFLAGS += -I$(HALIDE_ROOT)/include
+CFLAGS += -DNDEBUG -I$(HALIDE_ROOT)/include -std=c++11 #-restrict
 # LDFLAGS += -L$(HALIDE_ROOT)/lib -lHalideRuntime-host-cuda-debug
+CLANG = $(HOME)/.linuxbrew/opt/llvm/bin/clang++
+GPU_ARCH=sm_50
+CLANG_CUDA_LDFLAGS=-L/usr/local/cuda-8.0/lib64 -lcudart_static -ldl -lrt -pthread
 
 test: test.cu
-	nvcc -DNDEBUG $(CFLAGS) $(LDFLAGS) -O3 -arch sm_50 -std=c++11 -o $@ $<
+	nvcc $(CFLAGS) $(LDFLAGS) -O3 -arch $(GPU_ARCH) -o $@ $<
+
+test-llvm: test.cu
+	$(CLANG) -o $@ $< $(CFLAGS) $(LDFLAGS) -O3 --cuda-gpu-arch=sm_50 $(CLANG_CUDA_LDFLAGS)
+
+%.ptx: %
+	cuobjdump -ptx $< > $@.tmp
+	awk -vRS="\n\t" -vORS="" '1' $@.tmp > $@
+	rm $@.tmp
 
 run: test
-	./test
+	./test 10
 
 dbg: test
 	gdb ./test
 
+kernel-ptx: test.ptx
+	csplit --suffix-format='%02d.ptx' --prefix='test-' $< '/\.visible \.entry.*/' {*}
+
+llvm-kernel-ptx: test-llvm.ptx
+	csplit --suffix-format='%02d.ptx' --prefix='test-llvm-' $< '/\.visible \.entry.*/' {*}
+
 .PHONY: run dbg
+
--- test.cu
+++ test.cu
@@ -2,7 +2,63 @@
 #include <stdio.h>
 #include <assert.h>
 // #include <HalideRuntimeCuda.h>
+#ifndef __CUDA_ARCH__
+// host
 #include <HalideBuffer.h>
+#else
+// device
+#define HALIDE_ATTRIBUTE_ALIGN(x) __attribute__((aligned(x)))
+typedef struct buffer_t {
+    /** A device-handle for e.g. GPU memory used to back this buffer. */
+    uint8_t* __restrict__ dev;
+
+    /** A pointer to the start of the data in main memory. In terms of
+     * the Halide coordinate system, this is the address of the min
+     * coordinates (defined below). */
+    uint8_t* host;
+
+    /** The size of the buffer in each dimension. */
+    int32_t extent[4];
+
+    /** Gives the spacing in memory between adjacent elements in the
+    * given dimension.  The correct memory address for a load from
+    * this buffer at position x, y, z, w is:
+    * host + elem_size * ((x - min[0]) * stride[0] +
+    *                     (y - min[1]) * stride[1] +
+    *                     (z - min[2]) * stride[2] +
+    *                     (w - min[3]) * stride[3])
+    * By manipulating the strides and extents you can lazily crop,
+    * transpose, and even flip buffers without modifying the data.
+    */
+    int32_t stride[4];
+
+    /** Buffers often represent evaluation of a Func over some
+    * domain. The min field encodes the top left corner of the
+    * domain. */
+    int32_t min[4];
+
+    /** How many bytes does each buffer element take. This may be
+    * replaced with a more general type code in the future. */
+    int32_t elem_size;
+
+    /** This should be true if there is an existing device allocation
+    * mirroring this buffer, and the data has been modified on the
+    * host side. */
+    HALIDE_ATTRIBUTE_ALIGN(1) bool host_dirty;
+
+    /** This should be true if there is an existing device allocation
+    mirroring this buffer, and the data has been modified on the
+    device side. */
+    HALIDE_ATTRIBUTE_ALIGN(1) bool dev_dirty;
+
+    // Some compilers will add extra padding at the end to ensure
+    // the size is a multiple of 8; we'll do that explicitly so that
+    // there is no ambiguity.
+    HALIDE_ATTRIBUTE_ALIGN(1) uint8_t _padding[10 - sizeof(void *)];
+} buffer_t;
+
+#endif
+
 
 const int kernel_radius = 9;
 const int kernel_area = (kernel_radius*2+1)*(kernel_radius*2+1);
@@ -15,45 +71,97 @@ const int width = 6400,
 #define ALWAYS_INLINE __attribute__((always_inline))
 #endif
 
-template <typename T>
+template <typename T, bool static_addr=false>
 ALWAYS_INLINE inline
-__device__ T& dev_pixel(buffer_t &buf, int x, int y) {
+__device__ T& out_pixel(buffer_t buf, int x, int y) {
     T *data = (T*)buf.dev;
-    assert(buf.stride[0] == 1);
-    assert(buf.stride[1] > 6300 && buf.stride[1] < 6500);
-    int x_offset = (x - buf.min[0]);
-    int y_offset = (y - buf.min[1]) * buf.stride[1];
+    int x_min, y_min, x_stride, y_stride;
+    if (static_addr) {
+        assert(buf.stride[0] == 1);
+        assert(buf.stride[1] > 6300 && buf.stride[1] < 6500);
+        x_min = buf.min[0];
+        y_min = buf.min[1];
+        x_stride = 1;
+        y_stride = buf.stride[1];
+    } else {
+        x_min = 0;
+        y_min = 0;
+        x_stride = 1;
+        y_stride = width;
+    }
+    int x_offset = (x - x_min) * x_stride;
+    int y_offset = (y - y_min) * y_stride;
     return data[x_offset + y_offset];
 }
 
+template <typename T, bool static_addr=false>
+ALWAYS_INLINE inline
+const __device__ T in_pixel(const buffer_t buf, int x, int y) {
+    const T *data = (const T*)buf.dev;
+    int x_min, y_min, x_stride, y_stride;
+    if (static_addr) {
+        assert(buf.stride[0] == 1);
+        assert(buf.stride[1] > 6300 && buf.stride[1] < 6500);
+        x_min = buf.min[0];
+        y_min = buf.min[1];
+        x_stride = 1;
+        y_stride = buf.stride[1];
+    } else {
+        x_min = -kernel_radius;
+        y_min = -kernel_radius;
+        x_stride = 1;
+        y_stride = width+2*kernel_radius;
+    }
+    int x_offset = (x - x_min) * x_stride;
+    int y_offset = (y - y_min) * y_stride;
+    return __ldg(data + x_offset + y_offset);
+}
+
 __global__ void
-boxBlur(buffer_t in, buffer_t out) {
+boxBlurBuf(const buffer_t in, buffer_t out) {
     int x = blockIdx.x * blockDim.x + threadIdx.x + out.min[0];
     int y = blockIdx.y * blockDim.y + threadIdx.y + out.min[1];
 
+#if 0
     if (x < out.min[0] || y < out.min[1] ||
         x >= out.extent[0] || y >= out.extent[1])
     {
         return;
     }
+#endif
     
     float res = 0;
     for (int j = -kernel_radius; j <= kernel_radius; j++) {
         for (int i = -kernel_radius; i <= kernel_radius; i++) {
-            res += dev_pixel<float>(in, x+i, y+j);
+            res += in_pixel<float>(in, x+i, y+j);
         }
     }
-    res *= 1.f/kernel_area;
+    res /= float(kernel_area);
 
-    dev_pixel<float>(out, x,y) = res;
+    out_pixel<float>(out, x,y) = res;
+}
+
+__global__ void
+boxBlurBufStatic(const buffer_t in, buffer_t out) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x + out.min[0];
+    int y = blockIdx.y * blockDim.y + threadIdx.y + out.min[1];
+
+    float res = 0;
+    for (int j = -kernel_radius; j <= kernel_radius; j++) {
+        for (int i = -kernel_radius; i <= kernel_radius; i++) {
+            res += in_pixel<float,true>(in, x+i, y+j);
+        }
+    }
+    res /= kernel_area;
+
+    out_pixel<float,true>(out, x,y) = res;
 }
 
 // TODO: restrict on in/out DOUBLES performance!
-// #define _FLOAT(b) (reinterpret_cast<float*>((b).dev))
-#define OUT_PIXEL(x,y) (out[(x)+6400*(y)])
-#define IN_PIXEL(x,y) (in[((x)+kernel_radius)+6400*((y)+kernel_radius)])
+#define OUT_PIXEL(x,y) (out[(x)+width*(y)])
+#define IN_PIXEL(x,y) (in[((x)+kernel_radius)+width*((y)+kernel_radius)])
 __global__ void
-boxBlurStatic(const float * __restrict__ in, float * __restrict__ out) {
+boxBlurStatic(const float * __restrict__ in, float *out) {
 // boxBlurStatic(const float *in, float *out) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
@@ -68,10 +176,26 @@ boxBlurStatic(const float * __restrict__ in, float * __restrict__ out) {
 
     OUT_PIXEL(x,y) = res;
 }
-// #undef _FLOAT
+
+__global__ void
+boxBlurStaticNonRestrict(const float *in, float *out) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    float res = 0;
+    for (int j = -kernel_radius; j <= kernel_radius; j++) {
+        for (int i = -kernel_radius; i <= kernel_radius; i++) {
+            res += IN_PIXEL(x+i, y+j);
+        }
+    }
+    res /= float(kernel_area);
+
+    OUT_PIXEL(x,y) = res;
+}
 #undef OUT_PIXEL
 #undef IN_PIXEL
 
+#ifndef __CUDA_ARCH__
 template <typename T>
 void dev_malloc(Halide::Buffer<T> &buf) {
     buffer_t *b = buf.raw_buffer();
@@ -106,6 +230,30 @@ void host_to_dev(Halide::Buffer<T> &buf) {
     cudaMemcpy(dev, b->host, buf.size_in_bytes(), cudaMemcpyHostToDevice);
 }
 
+const int block_width = 32,
+          block_height = 32;
+dim3 blocks((width + block_width - 1) / block_width,
+            (height + block_height - 1) / block_height);
+dim3 threads(block_width, block_height);
+
+using std::vector;
+using std::string;
+using std::pair;
+
+// template<class ...Args>
+// void variant(std::string name, void(*kernel)(Args...), Args... args) {
+//     variants.push_back(
+//         std::make_pair(name, [&]{
+//             kernel<<<blocks, threads>>>(args...);
+//         })
+//     );
+// }
+#define variant(nm,...) (variants.push_back(std::make_pair( \
+#nm, [&]{\
+    (nm)<<<blocks,threads>>>(__VA_ARGS__); \
+} \
+)))
+
 int main (int argc, char const *argv[])
 {
     int trials = 1;
@@ -116,12 +264,6 @@ int main (int argc, char const *argv[])
                out(width, height);
 
     in.set_min(-kernel_radius, -kernel_radius);
-
-    const int block_width = 32,
-              block_height = 32;
-    dim3 blocks((width + block_width - 1) / block_width,
-                (height + block_height - 1) / block_height);
-    dim3 threads(block_width, block_height);
 
     in.for_each_element([&](int x, int y) {
         in(x, y) = (x % 3 == 0 && y % 3 == 0) ? 1.f : 0.f;
@@ -136,38 +278,51 @@ int main (int argc, char const *argv[])
     cudaEvent_t startEv, endEv;
     cudaEventCreate(&startEv);
     cudaEventCreate(&endEv);
-
-    cudaEventRecord(startEv);
-    for (int i = 0; i < trials; i++) {
-        // boxBlur<<<blocks, threads>>>(*in.raw_buffer(), *out.raw_buffer());
-        boxBlurStatic<<<blocks, threads>>>((float*)in.raw_buffer()->dev, (float*)out.raw_buffer()->dev);
-    }
-    cudaEventRecord(endEv);
-
-    dev_to_host(in);
-    dev_to_host(out);
-
-    in.for_each_element([&](int x, int y) {
-        if (y > 16) return;
-        if (x >= 0 && x < 16) printf("%.2f ", in(x, y));
-        if (x == 16) printf("\n");
-    });
-    printf("\n");
-    out.for_each_element([&](int x, int y) {
-        if (y > 16) return;
-        if (x >= 0 && x < 16) printf("%.2f ", out(x, y));
-        if (x == 16) printf("\n");
-    });
-
-    float elapsed;
-    cudaEventElapsedTime(&elapsed, startEv, endEv);
-    printf("\n-------\nTIME: %f ms / %d trials = %f ms\n", elapsed, trials, elapsed/trials);
-    int64_t pixels = width*height;
-    int64_t kernel_pixels = (kernel_radius*2+1)*(kernel_radius*2+1);
-    printf("Inputs accumulated: %ldM\n", pixels*kernel_pixels/1000000);
     
+    typedef std::function<void(void)> Fn;
+    vector<pair<string,Fn> > variants;
+    
+    variant(boxBlurBuf,
+            *(in.raw_buffer()), *(out.raw_buffer()) );
+    
+    variant(boxBlurBufStatic,
+            *(in.raw_buffer()), *(out.raw_buffer()) );
+    
+    variant(boxBlurStatic,
+            (float*)in.raw_buffer()->dev, (float*)out.raw_buffer()->dev);
+    
+    variant(boxBlurStaticNonRestrict,
+            (float*)in.raw_buffer()->dev, (float*)out.raw_buffer()->dev);
+
+    for (auto &variant : variants)
+    {
+        std::string name;
+        Fn fn;
+        std::tie(name, fn) = variant;
+        
+        cudaEventRecord(startEv);
+        for (int i = 0; i < trials; i++) {
+            fn();
+        }
+        cudaEventRecord(endEv);
+
+        dev_to_host(in);
+        dev_to_host(out);
+
+        float elapsed;
+        cudaEventElapsedTime(&elapsed, startEv, endEv);
+        printf( "\n-------\n"
+                "%s\n"
+                "TIME: %f ms / %d trials = %f ms\n",
+                name.c_str(), elapsed, trials, elapsed/trials );
+        int64_t pixels = width*height;
+        int64_t kernel_pixels = (kernel_radius*2+1)*(kernel_radius*2+1);
+        printf("Inputs accumulated: %ldM\n", pixels*kernel_pixels/1000000);
+    }
+
     dev_free(in);
     dev_free(out);
     
     return 0;
 }
+#endif //host-only
